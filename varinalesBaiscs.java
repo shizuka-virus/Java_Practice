a01e8ea42e61a942b0e164c4ba74256edf7d8070
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-08-10 20:18:28 +0500

    14

---
+++ varinalesBaiscs.java
@@ -0,0 +1,8 @@
+public class varinalesBaiscs {
+    public static void main(String[] args) {
+        int a = 12;
+        int b=14;
+        System.out.println(a+b);
+        // String nan="virus";
+    }
+}
