ad9c63244cde63321a83fcc5ed9d42518fd5f923
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-08-10 20:17:12 +0500

    2

---
+++ dataTypes.java
@@ -0,0 +1,14 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        byte a=8;
+        System.out.println(a);
+        char cha='a';
+        System.out.println(cha);
+        boolean bool=true;
+        System.out.println(bool);
+        double fl=3.9;
+        System.out.println(fl);
+        int num=4;
+        System.out.println(num);
+    }
+}
