8ec4355b38f30a08b1cf68682c23caa4458a496c
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-08-10 20:17:24 +0500

    3

---
+++ input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        // String in = sc.next();
+        // System.out.println(in);
+        // String fullName = sc.nextLine();
+        // System.out.println(fullName);
+        int num = sc.nextInt();
+        System.out.println(num);
+
+    }
+}
